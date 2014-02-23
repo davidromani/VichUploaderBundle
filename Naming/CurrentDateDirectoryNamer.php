689a0a9401a6a3ad7f60c93b42c4d3af9f47be4e
David Romaní <david@flux.cat>
2014-02-23 20:23:22 +0100

    current date directory name class created

---
+++ Naming/CurrentDateDirectoryNamer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Vich\UploaderBundle\Naming;
+
+/**
+ * CurrentDateDirectoryNamer
+ *
+ * @author David Romaní <david@flux.cat>
+ */
+class CurrentDateDirectoryNamer implements DirectoryNamerInterface
+{
+    /**
+     * Get current date directory name with format YYYY/mm/dd
+     *
+     * @param object $obj
+     * @param string $field
+     * @param string $uploadDir
+     *
+     * @return string format YYYY/mm/dd with current date
+     */
+    public function directoryName($obj, $field, $uploadDir)
+    {
+        $currentDate = new \DateTime();
+
+        return $currentDate->format('Y/m/d');
+    }
+}
